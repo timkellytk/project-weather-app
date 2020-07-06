391842272cab9f8a9cf46129d83e578502f52e18
Tim Kelly <timpkelly1@gmail.com>
2020-07-06 16:59:47 +1000

    Updated the fetch() API command for weather to use async and await

--- src/index.js
+++ src/index.js
@@ -1,44 +1,28 @@
 import moment from 'moment'
-import print from './print.js'
 import './style.css'
-
-
-const createText = (string) => {
-	const div = document.createElement('div')
-	const text = document.createElement('p')
-	text.textContent = string
-	div.appendChild(text)
-	return document.body.appendChild(div)
-}
-
-print()
 
 const localTimeConverter = (sec) => {
 	const date = new Date(sec * 1000);
 	return moment(date).format('LT')
 }
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=sydney&appid=7c19479f21a089bc2821f199dacb7a19&units=metric')
-	.then((response) => {
-		return response.json();
-	})
-	.then((response) => {
-		const weather = {
-			name: response.name,
-			description: response.weather[0].main,
-			date: moment().format('MMMM Do YYYY, h:mm a'),
-			temp: response.main.temp,
-			wind: response.wind.speed,
-			humidity: response.main.humidity,
-			tempMin: response.main.temp_min,
-			tempMax: response.main.temp_max,
-			sunrise: localTimeConverter(response.sys.sunrise),
-			sunset: localTimeConverter(response.sys.sunset),
-		}
-		return console.log(weather)
-	})
-	.catch(function (err) {
-		console.log(err)
-	});
+const getWeatherData = async () => {
+	const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=sydney&appid=7c19479f21a089bc2821f199dacb7a19&units=metric')
+	const data = await response.json()
+	const weather = {
+		name: data.name,
+		description: data.weather[0].main,
+		time: moment().format('h:mm a'),
+		temp: Math.round(data.main.temp),
+		feelsLike: Math.round(data.main.feels_like),
+		wind: Math.round(data.wind.speed),
+		tempMin: Math.round(data.main.temp_min),
+		tempMax: Math.round(data.main.temp_max),
+		humidity: data.main.humidity,
+		sunrise: localTimeConverter(data.sys.sunrise),
+		sunset: localTimeConverter(data.sys.sunset),
+	}
+	return weather
+}
 
-createText('This is a weather app')
+getWeatherData().then((response) => { console.log(response) })
